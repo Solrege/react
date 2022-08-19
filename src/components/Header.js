28afd9f3f0917618ad4c17ebd73d281ed44a0b2f
solrege <soledad.rege@gmail.com>
2022-08-18 22:18:19 -0300

    navBar

---
+++ src/components/Header.js
@@ -0,0 +1,24 @@
+
+
+
+const Header = () => {
+
+    return (
+        <div>
+            <header>
+                <h1 className="title"> Tienda de Cupcakes</h1>
+
+                <nav className="navBar">
+                    <a>Inicio</a>
+                    <a>About</a>
+                    <a>Productos</a>
+                    <a>Contacto</a>
+                    <a>Carrito</a>
+                </nav>
+            </header>
+        </div>
+
+    )
+}
+
+export default Header
